70f48ad15dc4e7e5ef93fce346fe790b892b90f1
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-08-26 16:39:12 -0300

    pronto pra começar

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # java__app_console_manipula_arquivos
 Aplicação console em Java que faz manipulação de arquivos
+
+
+Necessário Java 17
---
+++ src/main/java/br/com/manipulacao/arquivo/Main.java
@@ -0,0 +1,35 @@
+package br.com.manipulacao.arquivo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        menu_options_text();
+        menu_options_choice(in);
+    }
+
+    public static void menu_options_text(){
+        System.out.println("---------- OPÇÕES ----------");
+        System.out.println("1. Ler e exibir arquivo");
+        System.out.println("2. Escrever no arquivo");
+        System.out.println("3. Sair");
+        System.out.println("------------------------");
+    }
+    public static void menu_options_choice(Scanner in ) {
+        String option = in.nextLine();
+
+        while (!"3".equals(option)) {
+            switch (option) {
+                case "1": // ler arquivo
+                    break;
+                case "2": // escrever no arquivo
+                    break;
+                case "3":
+                    System.out.println("Até uma próxima");
+                    break;
+            }
+            option = in.nextLine();
+        }
+    }
+}
